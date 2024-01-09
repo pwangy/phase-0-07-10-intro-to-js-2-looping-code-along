d825f5978fc997d547b9b0953d532c50023664c1
pwangy <blipsandclicks@gmail.com>
2024-01-08 20:49:15 -0800

    writeCards passes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// for ([initialization]; [condition]; [iteration]) {
+//     [loop body]
+//   }
+
+function writeCards(names, eventName) {
+	let messages = []
+	for (let i = 0; i < names.length; i++) {
+		messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+	}
+
+	return messages
+}
+
+writeCards(['Charlie', 'Eva', 'Sam'], 'birthday')
+
+// function countDown() {
+// create a while loop
+// }
